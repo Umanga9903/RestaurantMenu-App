0db626dfa737f92d6fb4e089d188eb30da5ac06a
Umanga Luitel <letiul.umanga011.022@gmail.com>
2022-05-03 13:05:10 +0545

    uploading finished project

---
+++ src/component/basis/menuApi.js
@@ -0,0 +1,106 @@
+
+const Menu = [
+  {
+    id: 1,
+    image: "images/momo.jpg",
+    name: "momo",
+    category: "evening",
+    price: "Rs 150",
+    description:
+      "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+
+  {
+    id: 2,
+    image: "images/allupakoida.jpg",
+    name: "allu pakoida",
+    category: "evening",
+    price: "Rs 20",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 3,
+    image: "images/corn.jpg",
+    name: "corn",
+    category: "breakfast",
+    price: "Rs 25",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  
+  {
+    id: 4,
+    image: "../images/pizza.jpg",
+    name: "pizza",
+    category: "evening",
+    price: "Rs 180",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 5,
+    image: "../images/vegthali.jpg",
+    name: "Thali",
+    category: "dinner",
+    price: "Rs 180",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 6,
+    image: "../images/sweet.jpg",
+    name: "Sweets",
+    category: "dinner",
+    price: "Rs 60",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 7,
+    image: "../images/rajmarice.jpg",
+    name: "Rajma Rice",
+    category: "lunch",
+    price: "Rs 60",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 8,
+    image: "../images/samosa.jpg",
+    name: "samaso",
+    category: "evening",
+    price: "Rs 20",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 9,
+    image: "../images/rotiwithmeat.jpg",
+    name: "Rotiwithmeat",
+    category: "evening",
+    price: "Rs 120",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 10,
+    image: "../images/nonvegthali.jpg",
+    name: "Nonvegthali",
+    category: "evening",
+    price: "Rs 200",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 11,
+    image: "../images/nonvegthali.jpg",
+    name: "New",
+    category: "LTL",
+    price: "Rs 200",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+];
+
+export default Menu;
